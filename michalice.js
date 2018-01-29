c3313298e44f82ed52811ac6801a2c91dd10d8c0
Michael <michael.gaudin@ensg.eu>
2018-01-29 14:42:53 +0100

    Boutons rotation infinie

--- michalice.js
+++ michalice.js
@@ -1,4 +1,4 @@
-pitch45 = document.getElementById('pitch45');
+var pitch45 = document.getElementById('pitch45');
 
 pitch45.addEventListener('click', pitch45Handler);
 
@@ -6,7 +6,7 @@ function pitch45Handler(){
   globeView.controls.setTilt(45, true);
 }
 
-pitch315 = document.getElementById('pitch315');
+var pitch315 = document.getElementById('pitch315');
 
 pitch315.addEventListener('click', pitch315Handler);
 
@@ -14,27 +14,36 @@ function pitch315Handler(){
   globeView.controls.setTilt(-45, true);
 }
 
-reset = document.getElementById('reset');
+var reset = document.getElementById('reset');
 
 reset.addEventListener('click', resetHandler);
 
 function resetHandler(){
   globeView.controls.setOrbitalPosition({heading: 0, tilt: 0}, true);
+  position = 0;
 }
 
 
-head90 = document.getElementById('head90');
-
+var head90 = document.getElementById('head90');
+var position = 0;
 head90.addEventListener('click', head90Handler);
 
 function head90Handler(){
-  globeView.controls.setHeading(90, true);
-}
+  position += 90;
+  if (position == 360){
+    position = 0;
+  };
+  globeView.controls.setHeading(position, true);
+};
 
-head270 = document.getElementById('head270');
+var head270 = document.getElementById('head270');
 
 head270.addEventListener('click', head270Handler);
 
 function head270Handler(){
-  globeView.controls.setHeading(-90, true);
-}
+  position -= 90;
+  if (position == -360){
+    position = 0;
+  };
+  globeView.controls.setHeading(position, true);
+};
