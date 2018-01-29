1ce5b4acc88d28801400ad326e27a0c990c44252
Michael <michael.gaudin@ensg.eu>
2018-01-29 10:13:50 +0100

    Ajout boutons de développement

---
+++ michalice.js
@@ -0,0 +1,23 @@
+pitch45 = document.getElementById('pitch45');
+
+pitch45.addEventListener('click', pitch45Handler);
+
+function pitch45Handler(){
+  alert('Ca marche !')
+}
+
+pitch315 = document.getElementById('pitch315');
+
+pitch315.addEventListener('click', pitch315Handler);
+
+function pitch315Handler(){
+  alert('Ca marche 315 !')
+}
+
+reset = document.getElementById('reset');
+
+reset.addEventListener('click', resetHandler);
+
+function resetHandler(){
+  alert('Ca marche reset !')
+}
